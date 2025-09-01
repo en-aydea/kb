e01fa95262898f10e91d92d52e508272684adf16
en-aydea <enesaydin91@gmail.com>
2025-09-02 00:13:23 +0300

    import updated

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
-from tools import init_db, fetch_customer_profile, fetch_financials, rules_loan_eligibility
+from .tools import init_db, fetch_customer_profile, fetch_financials, rules_loan_eligibility
 
 app = FastAPI(title="Agentic Voice Banking")
 
